2327944436372b9286d7de5d14425207cdfa72d9
oleksandr-gr8 <124836099+oleksandr-gr8@users.noreply.github.com>
2023-02-09 17:53:08 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     ],targets: [
         .binaryTarget(
             name: "ModulorStyles",
-            url: "https://modulor.pmi-tech.io/ios/modulorstyles/binaries/Parimatch/1.1.2/ModulorStyles.xcframework.zip",
-            checksum: "ec1b3967fc985b6c129c2e315c7288f604b1dc8a5d1ec8a7dcd76e76305216a4")
+            url: "https://modulor.pmi-tech.io/ios/modulorstyles/binaries/Parimatch/4.67.0/ModulorStyles.xcframework.zip",
+            checksum: "4742c066e8ab49f4e6d0b5b6b9d5804be2b31e087764aaa84ca0fbe65ea8a199")
     ],
     swiftLanguageVersions: [.v5]
 )
